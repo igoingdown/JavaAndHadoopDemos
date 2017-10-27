53d410484be7dbfbfbc7edb98e6dfd547f27f5fa
igoingdown <fycjmingxing@126.com>
2017-10-27 16:38:35 +0800

    and commit time: 2017-10-27 16:38:35.

--- go_valley_class_problems/LuckyNumbers.java
+++ go_valley_class_problems/LuckyNumbers.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class LuckyNumbers {
     public static void main(String[] args) {
-        List<String> aList = luckyNumbers("13776032209", 888);
+        List<String> aList = luckyNumbers("2224", 888);
         for (String s : aList) System.out.println(s);
     }
 
@@ -18,21 +18,27 @@ public class LuckyNumbers {
 
     private static void recursion(String num, int target, int start, int cur, int last,
                                   List<String> res, String solution) {
+        // TODO: 注意last参数的处理。
+        // TODO: 注意start为0的情况的处理。
         if (start == num.length()) {
             if (target == cur) res.add(solution);
             return;
         }
         for (int i = start; i < num.length(); i++) {
-            String numString = num.substring(start, i);
-            if (num.charAt(start) == '0' && i > start) continue;
+            String numString = num.substring(start, i + 1);
+            if (num.charAt(start) == '0' && i != start) break;
             int numInt = Integer.parseInt(numString);
-            recursion(num, target, i + 1, cur + numInt, -numInt, res,solution + '+' + numString);
-            recursion(num, target, i + 1, cur - numInt, numInt, res, solution + '-' + numString);
-            recursion(num, target, i +1, cur + last + last * numInt, -last * numInt,
-                    res, solution + '*' + numString);
-            if (numInt > 0 && last % numInt == 0) {
-                recursion(num, target, i + 1, cur + last + last * numInt,
-                        -last / numInt, res, solution + "/" + numString);
+            if (start == 0) {
+                recursion(num, target, i + 1, numInt, numInt, res, solution + numString);
+            } else {
+                recursion(num, target, i + 1, cur + numInt, numInt, res,solution + '+' + numString);
+                recursion(num, target, i + 1, cur - numInt, - numInt, res, solution + '-' + numString);
+                recursion(num, target, i +1, cur - last + last * numInt, last * numInt,
+                        res, solution + '*' + numString);
+                if (numInt > 0 && last % numInt == 0) {
+                    recursion(num, target, i + 1, cur - last + last * numInt,
+                            last / numInt, res, solution + "/" + numString);
+                }
             }
         }
     }
---
+++ java_basic_tools/StringTest.java
@@ -0,0 +1,8 @@
+package java_basic_tools;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String s = new String("hello world");
+        System.out.println(s.substring(0, 1));
+    }
+}
