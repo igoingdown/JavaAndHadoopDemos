54cc4a1a66d2f73acd120022dc4cfa9527e64bd7
igoingdown <fycjmingxing@126.com>
2017-11-01 10:46:27 +0800

    and commit time: 2017-11-01 10:46:27.

--- go_valley_class_problems/ZeroOneKnapsackWithValue.java
+++ go_valley_class_problems/ZeroOneKnapsackWithValue.java
@@ -1,5 +1,26 @@
 package go_valley_class_problems;
 
 public class ZeroOneKnapsackWithValue {
+    public static void main(String[] args) {
+        int[] weights = {1, 3, 4, 5};
+        int[] values = {3, 8, 4, 7};
+        System.out.println(maxValue(weights, values, 1));
+    }
+
+    public static int maxValue(int[] weights, int[] values, int target) {
+        int m = weights.length;
+        int[][] dp = new int[m + 1][target + 1];
+        for (int i = 1; i < m + 1; i++) {
+            int itemIndex = i - 1;
+            for (int j = 1; j < target + 1; j++) {
+                if (weights[itemIndex] <= j) {
+                    dp[i][j] = Math.max(values[itemIndex] + dp[i - 1][j - weights[itemIndex]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[m][target];
+    }
 
 }
