83e40ea9d6b2014af9a7b95276dfc3e1150c7dbf
igoingdown <fycjmingxing@126.com>
2017-11-02 00:36:42 +0800

    and commit time: 2017-11-02 00:36:42.

---
+++ go_valley_class_problems/LongestCommonSequence.java
@@ -0,0 +1,22 @@
+package go_valley_class_problems;
+
+public class LongestCommonSequence {
+    public static void main(String[] args) {
+        String a = "abcfbc", b = "abfcab";
+        System.out.println(lcs(a, b));
+    }
+
+    public static int lcs(String a, String b) {
+        int[][] dp = new int[a.length() + 1][b.length() + 1];
+        for (int i = 1; i < a.length() + 1; i++) {
+            for (int j = 1; j < b.length() + 1; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[a.length()][b.length()];
+    }
+}
