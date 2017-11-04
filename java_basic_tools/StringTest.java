1c5c90d28438ca6cf47e8560e2f26016626aaef6
igoingdown <fycjmingxing@126.com>
2017-11-05 01:16:00 +0800

    and commit time: 2017-11-05 01:16:00.

--- java_basic_tools/StringTest.java
+++ java_basic_tools/StringTest.java
@@ -4,6 +4,7 @@ public class StringTest {
     public static void main(String[] args) {
         String s = new String("hello world");
         System.out.println(s.substring(0, 1));
+        System.out.println(s.substring(1, 2));
         System.out.println(s.substring(1));
     }
 }
