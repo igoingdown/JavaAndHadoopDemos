1a8c90ec1582e9b28c0b12640be210d68103eb3c
igoingdown <fycjmingxing@126.com>
2017-11-05 14:20:22 +0800

    test string  and commit time: 2017-11-05 14:20:22.

--- java_basic_tools/StringTest.java
+++ java_basic_tools/StringTest.java
@@ -5,6 +5,10 @@ public class StringTest {
         String s = new String("hello world");
         System.out.println(s.substring(0, 1));
         System.out.println(s.substring(1, 2));
+
+        String a=  "hh";
+        String b = "hh";
+        System.out.println(a == b);
         System.out.println(s.substring(1));
     }
 }
