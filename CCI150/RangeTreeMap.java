88b0d5e9ddd03db50b4045192f7318e6c40c2341
igoingdown <fycjmingxing@126.com>
2018-05-16 00:50:13 +0800

    get offer  and commit time: 2018-05-16 00:50:13.

--- CCI150/RangeTreeMap.java
+++ CCI150/RangeTreeMap.java
@@ -1,5 +1,43 @@
 package CCI150;
 
-public class RangeTreeMap {
+import java.util.TreeMap;
 
+public class RangeTreeMap {
+    private TreeMap<Integer, Integer> map = new TreeMap<>();
+
+    public void insertRange(int left, int right) {
+        if (left >= right) return;
+        Integer start = map.floorKey(left);
+        if (start == null) start = map.ceilingKey(left);
+        while (start != null && start <= right) {
+            int end = map.get(start);
+            if (end >= left) {
+                map.remove(start);
+                if (start < left) left = start;
+                if (end > right) right = end;
+            }
+            start = map.ceilingKey(end);
+        }
+        map.put(left, right);
+    }
+
+    public boolean queryRange(int left, int right) {
+        Integer start = map.floorKey(left);
+        return start != null && map.get(start) >= right;
+    }
+
+    public void removeRange(int left, int right) {
+        if (left >= right) return;
+        Integer start = map.floorKey(left);
+        if (start == null) start = map.ceilingKey(left);
+        while (start != null && start < right) {
+            int end = map.get(start);
+            if (end >= left) {
+                map.remove(start);
+                if (start < left) map.put(start, left);
+                if (end > right) map.put(right, end);
+            }
+            start = map.ceilingKey(end);
+        }
+    }
 }
