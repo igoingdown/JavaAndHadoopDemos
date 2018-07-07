cb85bfde34327c4f04e600e5a672d965471a5a53
igoingdown <fycjmingxing@126.com>
2018-07-07 22:04:52 +0800

    kill hard  and commit time: 2018-07-07 22:04:52.

--- go_valley_class_problems/Maze.java
+++ go_valley_class_problems/Maze.java
@@ -25,7 +25,9 @@ public class Maze {
         } else {
             System.out.println("no");
         }
-        System.out.println("");
+        if (solveMazeNoVisit(board1, 0, 0, 4, 4)) System.out.println("yes");
+        else System.out.println("no");
+        System.out.println();
     }
     private static boolean solveMaze(char[][] board, int startX, int startY, int endX, int endY, boolean[][] visited) {
         if (startX >= board.length || startY >= board[0].length || startX < 0 || startY < 0 ||
@@ -54,9 +56,9 @@ public class Maze {
         // 本题不需要回溯，这是因为本题需要返回的结果是bool值，就是说能不能走通，
         //     如果DFS过程中，经过一个节点的第一个path走过这个节点时，通过遍历其后续所有可能的path得知这个节点无法走通
         //     则其他分支即使有可能经过节点也不必再次遍历，因为其后面的所有可能都被遍历过了！
-        // 而回溯法(所有子问题遍历结束后使用"board[startX][startY] = '.' "将board状态还原)的递归深度是定值，
-        //     子问题规模是定值，所以一定不会出现无限递归！这一点助教说错了！
-        // 这也是MazeII那道题要用回溯法的原因。
+        // 而回溯法(所有子问题遍历结束后使用"board[startX][startY] = '.' "将board状态还原到遍历子问题之前)的递归深度是定值，
+        //     子问题规模是定值，所以一定不会出现无限递归！
+        // 这是能否到达终点问题和遍历到达终点所有可能路径问题的区别。
         for (int i = 0; i < 4; i++) {
             int newX = startX + bias[i], newY = startY + bias[i + 1];
             if (solveMazeNoVisit(board, newX, newY, endX, endY)) return true;
